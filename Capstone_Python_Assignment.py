752c1628f59f9b87195683bc39a7ad14b9b796c8
Anitha-Nagaraj <eceanee@gmail.com>
2020-01-19 01:31:32 -0500

    Simple notebook illustrated for GitHub integration

---
+++ Capstone_Python_Assignment.py
@@ -0,0 +1,8 @@
+# Project Capstone
+### This simple notebook is created to sahre on gitHub as an assignment on Capstone
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone project course")
+
